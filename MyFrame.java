21c11ff936aa84f78816ac7cd5135fdcb208efd4
osamah <osamah@osamahs-MacBook-Pro-2.local>
2023-04-07 16:45:18 +0300

    fix frame

--- MyFrame.java
+++ MyFrame.java
@@ -5,23 +5,41 @@ import javax.swing.*;
 class MyFrame extends JFrame {
 
     MyFrame() {
+        //body JPanel
         JPanel bodyPanel = new JPanel();
-        // bodyPanel.setBackground(Color.WHITE);
-        bodyPanel.setBounds(15, 200, 260, 320);
-        bodyPanel.add(myJButton("1", 0, 200));
-        bodyPanel.add(myJButton("2", 100, 200));
-        bodyPanel.add(myJButton("3", 200, 200));
-        bodyPanel.add(myJButton("4", 0, 100));
-        bodyPanel.add(myJButton("5", 100, 100));
-        bodyPanel.add(myJButton("6", 200, 100));
-        bodyPanel.add(myJButton("7", 0, 0));
-        bodyPanel.add(myJButton("8", 100, 0));
-        bodyPanel.add(myJButton("9", 200, 0));
+        bodyPanel.setBackground(Color.RED);
+        bodyPanel.setBounds(20, 215, 259, 320);
 
+        
+
+
+        bodyPanel.add(myJButton("test", 0, 200));
+        bodyPanel.add(myJButton("test", 50, 200));
+        bodyPanel.add(myJButton("test", 100, 200));
+        bodyPanel.add(myJButton("test", 150, 200));
+
+        //other bottons
+        bodyPanel.add(myJButton("test", 0, 150));
+        bodyPanel.add(myJButton("test", 50, 150));
+        bodyPanel.add(myJButton("test", 100, 150));
+        bodyPanel.add(myJButton("test", 150, 150));
+        //the start of 1 to 9
+        bodyPanel.add(myJButton("1", 0, 100));
+        bodyPanel.add(myJButton("2", 50, 100));
+        bodyPanel.add(myJButton("3", 100, 100));
+        bodyPanel.add(myJButton("4", 0, 50));
+        bodyPanel.add(myJButton("5", 50, 50));
+        bodyPanel.add(myJButton("6", 100, 50));
+        bodyPanel.add(myJButton("7", 0, 0));
+        bodyPanel.add(myJButton("8", 50, 0));
+        bodyPanel.add(myJButton("9", 100, 0));
+        //the end of 1 to 9
+
+        //the frame
         this.setTitle("osamah's calculator");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
-        this.setSize(290, 570);
+        this.setSize(298, 669);
         this.setVisible(true);
         this.add(bodyPanel);
     }
@@ -29,7 +47,7 @@ class MyFrame extends JFrame {
     JButton myJButton(String lable, int x, int y) {
 
         JButton button = new JButton();
-        button.setBounds(x, y, 60, 60);
+        button.setBounds(x, y, 55, 55);
         button.setText(lable);
         button.setFocusable(false);
         return button;
